5dfbfced1e3fdef18c2bef38fc7c71724f9128dd
Sergey <73191202+Levchenski@users.noreply.github.com>
2021-07-09 21:56:33 +0300

    0.0.16

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-function moneyConverter(num){
-    document.getElementById('outputrub').innerHTML =  num*74;
+function mas(els) {
+    const a = [4, 5, 6];
+    let element = document.querySelectorAll('.item');
+    for(let s = 0; s < )
+    els(element, a);
 }
+
+function callback(elem, arr){
+    elem.innerHTML = arr.join('-')
+}
+
+mas(callback);
